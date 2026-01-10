7cc308fd2b39ff2a08c96510dc5022b8f03edfc2
ouissAb <aboujid.ouissam@gmail.com>
2026-01-10 23:54:53 +0100

    Ajout de l'app

---
+++ DevOps/src/main/java/com/plateforme/DevOpsApplication.java
@@ -0,0 +1,15 @@
+package com.plateforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+
+	}
+
+}
